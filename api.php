a8c939572453d51afebc3cecec99e5e34ce399f5
Christianfreitas <freitaschristian1204@gmail.com>
2022-07-04 17:51:10 -0300

    confirma

--- api.php
+++ api.php
@@ -1,7 +1,6 @@
 <?php
 
  function getAddress () {
-    //nesse if ele só vai fazer o codigo se o post do ce já ter feito.
     if ( isset ($_POST['cep'])){
         $cep= $_POST['cep']; 
 
@@ -9,7 +8,6 @@
 
             if (preg_match('/^[0-9]{8}$/', $cep) ){
                 $endereco = getViacep($cep);
-                // property_exists Verifica se o objeto ou classe tem uma propriedade
                 if (property_exists($endereco, 'erro') ){
                     $endereco = emptyFields();
                     $endereco->cep = "CEP não encontrado!";
@@ -36,14 +34,11 @@ function emptyFields(){
 } 
 
 function cleanCEP (String $cep):String {
-    //pega tudo o que é diferente do 0 a 9 e transforma em vazio 
     return preg_replace('/[^0-9]/', '', $cep);
 }
 
 function getViacep (string $cep){
     $url= "https://viacep.com.br/ws/{$cep}/json/";
-    //json_decode — Decodifica uma string JSON
-    //file_get_contents — Lê todo o conteúdo de um arquivo para uma string
     return json_decode(file_get_contents($url));
 }
 
